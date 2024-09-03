322548c7d5af6c663f0886ff92241df82c67d2e1
GenAI-BK <161469326+GenAI-BK@users.noreply.github.com>
2024-09-03 15:31:43 +0530

    Add files via upload

--- gemini-1.5.py
+++ gemini-1.5.py
@@ -1,15 +1,20 @@
+import google.generativeai as genai
 import PyPDF2
-from openai import OpenAI
-import PyPDF2
+from PIL import Image
 import streamlit as st
 import pandas as pd
 import sqlite3
+import io
 import base64
+import pdfplumber
 
-llm=OpenAI(api_key=st.secrets['OPENAI_API_KEY'])
+api_key = st.secrets["GOOGLE_API_KEY"]
 
 conn = sqlite3.connect('claim.db')
 cursor = conn.cursor()
+
+genai.configure(api_key=api_key)
+llm = genai.GenerativeModel("gemini-1.5-pro")
 
 policyholder_docs = {
     "John Doe": "C:/Users/jugal.gurnani/Downloads/insurance_claim/Policyholder_Document_John.pdf",
@@ -20,6 +25,16 @@ policyholder_docs = {
     "Trevor Andre": "C:/Users/jugal.gurnani/Downloads/insurance_claim/Policyholder_Document_Trevor.pdf"
     # Add more mappings as needed
 }
+
+def excel_to_db(csv_file, db_file, table_name):
+    # Read CSV file into a pandas DataFrame
+    df = pd.read_excel(csv_file)
+    # Create a connection to the SQLite database
+    conn = sqlite3.connect(db_file)
+    # Write DataFrame to SQLite database
+    df.to_sql(table_name, conn, if_exists='replace', index=False)
+    # Close the connection
+    conn.close()
 
 def convert_pdf_to_text(pdf):
     pdf_reader = PyPDF2.PdfReader(pdf)
@@ -35,6 +50,11 @@ def display_pdf(file_path):
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="500" type="application/pdf"></iframe>'
     return pdf_display
 
+if 'pdf1' not in st.session_state:
+    st.session_state.pdf1 = None
+if 'pdf2' not in st.session_state:
+    st.session_state.pdf2 = None
+
 cursor.execute("SELECT DISTINCT Name FROM claim_data")
 names = [row[0] for row in cursor.fetchall()]
 
@@ -48,10 +68,12 @@ if selected_name:
     cursor.execute('SELECT * FROM claim_data WHERE Policy_No = ?', (policy_number))
     final=cursor.fetchall()
     
+    
     if policy_number:
         st.write(f"Policy Number: {policy_number[0]}")
     else:
         st.write("No policy number found for the selected name.")
+
     
     if selected_name in policyholder_docs:
         policy_document_path = policyholder_docs[selected_name]
@@ -66,9 +88,7 @@ medical_bills=st.file_uploader("Upload Bills in a combined PDF", type="PDF")
 
 
 def gen_res(policy_terms_text, claim_form_text, final, bills):
-    response=llm.chat.completions.create(
-        model='gpt-4o-mini',
-        messages=[{"role":"user", "content":f'''You are an employee of claims department of a health insurance company.\
+    response=llm.generate_content([f'''You are an employee of claims department of a health insurance company.\
         The data provided to you is the data of a policyholder along with claim form and policyholder\
         terms and conditions. Firstly, check if all the necessary data is filled in the claim form,\
         which includes, Policy number, Policyholder name, Insured Members details, Date, Patient's name,\
@@ -91,9 +111,9 @@ def gen_res(policy_terms_text, claim_form_text, final, bills):
         Claim Form: {claim_form_text}
         Data:{final}
         Bills: {bills}
-        '''}],
-    )
-    return response.choices[0].message.content
+        ''']
+        )
+    return response.text
 
 if st.button('Submit'):
     if claim_form and medical_bills is not None and policy_terms_text:
